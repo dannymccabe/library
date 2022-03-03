550eba577a157d1d7c2d470f7f69e5cf87785496
Danny <danny.affiliatemarketing@gmail.com>
2022-03-02 21:50:04 -0300

    .

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//#################### library specific JavaScript ################################
 // Book constructor
 function Book(title, author, pages, read){
     this.title = title
@@ -18,4 +19,39 @@ let myLibrary = [];
 // Accept user input and store
 function addBookToLibrary(){
     
+}
+
+//#################### Pop-Up JavaScript ################################ DOES NOT WORK
+const openPopUpButton = document.querySelectorAll('[data-form-target]')
+const closePopUpButton = document.querySelectorAll('[data-close-btn]')
+const overlay = document.getElementsById('#overlay')
+
+//actions
+openPopUpButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const popUp = document.getElementsByClassName(".pop-up")
+        openPopUp(popUp)
+    })
+})
+
+closePopUpButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const popUp = document.getElementsByClassName('.pop-up')
+        closePopUp(popUp)
+    })
+})
+
+// change class of popUp and overlay to active to engage css
+function openPopUp(popUp){
+    if(popUp === null) return
+    popUp.classList.add('active')
+    overlay.classList.add('active')
+}
+
+// change class of popUp and overlay from active to engage css
+
+function closePopUp(popUp){
+    if(popUp === null) return
+    popUp.classList.remove('active')
+    overlay.classList.remove('active')
 }
